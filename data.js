8de015541ff6eb7af70cb66d0f1850bf3853e93b
clementblois-netizen <clementblois@gmail.com>
2026-06-29 14:07:13 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,322 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-la-grande-coure-de-rcration-vue-centrale",
+      "name": "La grande coure de récréation (vue centrale)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": -0.7375913186688976,
+        "pitch": 0.13961385997732556,
+        "fov": 1.1678810529903434
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0891480556446442,
+          "pitch": 0.02546252202173349,
+          "rotation": 6.283185307179586,
+          "target": "1-le-couloir-et-lescalier-de-la-grande-coure-de-rcration"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-le-couloir-et-lescalier-de-la-grande-coure-de-rcration",
+      "name": "Le couloir et l'escalier de la grande coure de récréation",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": 3.1142969125020867,
+        "pitch": 0.1761569388332962,
+        "fov": 1.1678810529903434
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8737385138921825,
+          "pitch": -0.2303220060419715,
+          "rotation": 0,
+          "target": "2-le-couloire-en-haut-de-la-grande-coure"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-le-couloire-en-haut-de-la-grande-coure",
+      "name": "Le couloire en haut de la grande coure",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": -0.14683841435102352,
+        "pitch": 0.3294972224650241,
+        "fov": 1.1678810529903434
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-la-grande-coure-de-rcration-vue-loigne",
+      "name": "La grande coure de récréation (vue éloignée)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": 0.8682870181606503,
+        "pitch": -0.04560368952577676,
+        "fov": 1.1678810529903434
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-la-grande-coure-de-rcration-vue-arrire",
+      "name": "La grande coure de récréation (vue arrière)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": 0.14438061286996628,
+        "pitch": -0.22612903200555934,
+        "fov": 1.1678810529903434
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.03915677606316059,
+          "pitch": -0.050116836048694324,
+          "rotation": 0,
+          "target": "5-le-prau"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-le-prau",
+      "name": "Le préau",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": -0.7123778174656472,
+        "pitch": 0.32883892256322156,
+        "fov": 1.1678810529903434
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4395352303216544,
+          "pitch": 0.05330507479200719,
+          "rotation": 0,
+          "target": "1-le-couloir-et-lescalier-de-la-grande-coure-de-rcration"
+        },
+        {
+          "yaw": 0.5060204093974114,
+          "pitch": 0.159770320315884,
+          "rotation": 0,
+          "target": "6-le-bureau-de-la-directrice"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-le-bureau-de-la-directrice",
+      "name": "Le bureau de la directrice",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": 0.985730366188946,
+        "pitch": 0.0565103718955946,
+        "fov": 1.1678810529903434
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.067005407473693,
+          "pitch": 0.18913156433533018,
+          "rotation": 0,
+          "target": "7-la-cantine"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-la-cantine",
+      "name": "La cantine",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": -2.493147928615244,
+        "pitch": 0.25101279070262095,
+        "fov": 1.1678810529903434
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-la-petite-coure-de-rcration",
+      "name": "La petite coure de récréation",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": -0.23675770722707767,
+        "pitch": 0.03324139523267888,
+        "fov": 1.1678810529903434
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-la-petite-coure-de-rcration-vue-loigne",
+      "name": "La petite coure de récréation (vue éloignée)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": 0.6806784082777888,
+        "pitch": 0.17950353425654342,
+        "fov": 1.1678810529903434
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Viste de l'école élémentaire Camille Flammarion",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
